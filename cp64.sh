65745e2e101c205c5f6c336378b6af3c0a5a442b
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-04-28 16:28:00 +0200

    Added -c option to run64.sh

---
+++ cp64.sh
@@ -0,0 +1,3 @@
+:
+# copy the file to SD card on  Nintendo 64
+./run64.sh -c
--- run64.sh
+++ run64.sh
@@ -4,8 +4,15 @@
 # This script is for Windows WSL (Windows Subsystem for Linux), since USB64 is a Windows tool.
 z64file=smsPlus64.z64
 [ -f $z64file ] || { echo "$z64file not found"; exit 1; }
-echo "Running $z64file on real hardware"
 # Note: usb64.exe must be in the same directory as this script and smsPlus64.z64, otherwise the rom will not start.
 # Also, the usb cable must be connected to the N64 and the PC and the console must be in the everdrive menu.
-./usb64.exe -rom=$z64file -start
+# check optional commandline parameter -c for copy only.
+if [ "$1" == "-c" ]; then
+    echo "Copying $z64file to Nintendo 64"
+    ./usb64.exe -cp $z64file sd:$z64file
+else
+  
+    echo "Running $z64file on real hardware"
+    ./usb64.exe -rom=$z64file -start
+fi
 # End of run64.sh
