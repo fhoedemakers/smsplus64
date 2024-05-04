f4f622ce26e89e3dc223ccec74e889e5eabd6dd9
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-05-04 08:11:22 +0200

    build.sh disables access to Everdriv's SD

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 :
 # Build release version of the project
 make clean
+echo "Building release version of the project"
+echo "make RELEASE=1 disables access to the Everdrive64's SD card."
+echo "Running debug version of the project is recommended."
 make RELEASE=1
--- build_debug.sh
+++ build_debug.sh
@@ -1,4 +1,6 @@
 :
 # Build debug version of the project
+# 
+echo "Building debug version of the project"
 make clean
 make D=1
