2920ea23cf4e1b3ae0bb2c17dc10262f88427097
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-05-04 11:07:28 +0200

    Removed comments

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 # Build release version of the project
 make clean
 echo "Building release version of the project"
-echo "make RELEASE=1 disables access to the Everdrive64's SD card. (because of -NDEBUG flag)"
-echo "Running debug version of the project is recommended."
 make RELEASE=1
