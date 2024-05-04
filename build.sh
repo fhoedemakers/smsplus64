8ff089839e13b91db6326570b9f511efc496f5b7
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-05-04 08:22:03 +0200

    Update build.sh to clarify that RELEASE=1 disables access to Everdrive64's SD card

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 # Build release version of the project
 make clean
 echo "Building release version of the project"
-echo "make RELEASE=1 disables access to the Everdrive64's SD card."
+echo "make RELEASE=1 disables access to the Everdrive64's SD card. (because of -NDEBUG flag)"
 echo "Running debug version of the project is recommended."
 make RELEASE=1
