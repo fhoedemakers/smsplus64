d21be5bd6db4b28eb71442ff8f57546afc6e6b02
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2024-11-25 15:21:55 +0100

    Refactor color definitions: move color macros to common.h and update menu.cpp

--- common.h
+++ common.h
@@ -25,4 +25,10 @@ typedef unsigned char  BYTE;
 #endif /* !NULL */
 
 #define RGB888_TO_RGB5551(r, g, b) (((r >> 3) << 11) | ((g >> 3) << 6) | ((b >> 3) << 1) | 1)
+#define CBLACK (RGB888_TO_RGB5551(0, 0, 0))
+#define CWHITE (RGB888_TO_RGB5551(255, 255, 255))
+#define CRED (RGB888_TO_RGB5551(255, 0, 0))
+#define CGREEN (RGB888_TO_RGB5551(76, 255, 0))
+#define CBLUE (RGB888_TO_RGB5551(0, 38, 255))
+#define CLIGHTBLUE (RGB888_TO_RGB5551(0, 148, 255))
 #endif /* !nytypes_TYPES_H_INCLUDED */
